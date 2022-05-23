b07a4dd4a6fd774f3b4ed33e8448c092feee93b4
Michael Elabd <mhelabd@stanford.edu>
2022-05-23 16:39:24 -0700

    visualization v0.1

--- eval.py
+++ eval.py
@@ -1,8 +1,9 @@
 import numpy as np
 import torch
+import pandas as pd
 from builtins import breakpoint
 from fairness import BinaryClassificationBiasDataset
-
+from explainability import ModelExplainer
 
 def eval(model, dataset, args, verbose=False):
     """
@@ -13,6 +14,8 @@ def eval(model, dataset, args, verbose=False):
     print("eval (acc): ", eval_accuracy(model, dataset, args, verbose))
     print("eval (fairness): ")
     eval_fairness(model, dataset, args, verbose=True)
+    print("eval (explainer): ")
+    eval_explainability(model, dataset, args, verbose=True)
 
 
 # protected class: gender (male: 1, female: 0)
@@ -27,7 +30,7 @@ def eval_accuracy(model, dataset, args, verbose=False):
     return accuracy_class0, accuracy_class1
 
 
-def eval_fairness(model, dataset, args, verbose=True):
+def eval_fairness(model, dataset, args, verbose=True, save=True):
     """
     Computes predictive parity for the different protected classes specified in args.protected_classes
     """
@@ -44,17 +47,26 @@ def eval_fairness(model, dataset, args, verbose=True):
     y_test1 = y_test * (X_protected)
 
     for i, (y_hat, y_test) in enumerate([(y_hat0, y_test0), (y_hat1, y_test1), (y_hat, y_test)]):
+        if verbose:
+            if i == 2: print("BOTH CLASSES")
+            else: print("CLASS ", i)
+
         fairness_eval = BinaryClassificationBiasDataset(
             preds=y_hat, labels=y_test, positive_class_favored=False)
-        if i == 2: print("BOTH CLASSES")
-        else: print("CLASS ", i)
+        if save:
+            df = pd.DataFrame(columns=fairness_metrics, index=[0, 1, 2])
         for metric in fairness_metrics:
             eval = fairness_eval.get_bias_result_from_metric(metric)
             if verbose: print(metric, ": ", eval)
+            if save: df.at[i, metric] = eval
+    if save:
+        print(df.head())
+        df.to_excel('./results/eval_metrics.xlsx', sheet_name='sheet1', index=False)
+        exit()
 
-def eval_robustness(model, dataset, args, verbose=False):
+def eval_explainability(model, dataset, args, verbose=False):
     """
     Computes robustness metrics  for the different protected classes specified in args.protected_classes
     """
-    # TODO
-    pass
+    model_explainer = ModelExplainer(model, dataset, args)
+    model_explainer.explain_model()
