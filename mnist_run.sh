45798d73314e87af2032f3f6c251c4876c4c69e9
Emily Wen <wenemily00@gmail.com>
2022-05-24 13:49:30 -0700

    oops the script

---
+++ mnist_run.sh
@@ -0,0 +1,2 @@
+python exp.py --dataset='mnist' --dataset_split=0.01 --al-proposal-size=300 --al-iters=50 --feature-distribution 0.1 0.1 0.1 0.1 0.18 0.1 0.1 0.1 0.1 0.02
+python exp.py --dataset='mnist' --dataset_split=0.01 --al-proposal-size=300 --al-iters=50 --al-method=entropy --feature-distribution 0.1 0.1 0.1 0.1 0.18 0.1 0.1 0.1 0.1 0.02
