8f470eb754e17bca2535a9fb616b816989448d0e
Emily Wen <wenemily00@gmail.com>
2022-05-24 12:31:50 -0700

    eval fairness

--- models.py
+++ models.py
@@ -32,6 +32,7 @@ def init_model(args):
 class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
+        self.num_classes = 10
         self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
         self.conv2_drop = nn.Dropout2d()
